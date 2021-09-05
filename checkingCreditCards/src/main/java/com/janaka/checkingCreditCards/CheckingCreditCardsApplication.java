cc3a04919fadab1b50a2224d678910d502a382ac
Janaka Dassanayake <niroshanjnd@gmail.com>
2021-09-05 22:19:32 +1000

    taskid - initial commit for checking credit cards

---
+++ checkingCreditCards/src/main/java/com/janaka/checkingCreditCards/CheckingCreditCardsApplication.java
@@ -0,0 +1,13 @@
+package com.janaka.checkingCreditCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingCreditCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckingCreditCardsApplication.class, args);
+	}
+
+}
---
+++ checkingCreditCards/src/test/java/com/janaka/checkingCreditCards/CheckingCreditCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janaka.checkingCreditCards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingCreditCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
