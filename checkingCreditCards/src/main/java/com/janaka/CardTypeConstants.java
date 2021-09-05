2416c8ef7ff2ed97e71f076632416424aba073ae
Janaka Dassanayake <niroshanjnd@gmail.com>
2021-09-06 01:04:22 +1000

    taskid cardtype constants file

---
+++ checkingCreditCards/src/main/java/com/janaka/CardTypeConstants.java
@@ -0,0 +1,17 @@
+package com.janaka;
+
+public class CardTypeConstants {
+	
+	public static final String AMEX = "AMEX";
+	
+	public static final String VISA = "Visa";
+	
+	public static final String MASTERCARD = "MasterCard";
+	
+	public static final String DISCOVER = "Discover";
+	
+	public static final String UNKNOWN = "Unknown";
+	
+	
+
+}
